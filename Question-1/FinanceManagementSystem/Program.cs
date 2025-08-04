2941ccb08bc40a257fbe11698bf94dea573f8237
Javis22Spe <marksspe.20@gmail.com>
2025-08-04 15:38:54 +0000

    Done with Question 1

--- Question-1/FinanceManagementSystem/Program.cs
+++ Question-1/FinanceManagementSystem/Program.cs
@@ -1,2 +1,125 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinanceManagementSystem
+{
+    // a. Define Transaction record
+    public record Transaction(int Id, DateTime Date, decimal Amount, string Category);
+
+    // b. Define ITransactionProcessor interface
+    public interface ITransactionProcessor
+    {
+        void Process(Transaction transaction);
+    }
+
+    // c. Implementations of ITransactionProcessor
+    public class BankTransferProcessor : ITransactionProcessor
+    {
+        public void Process(Transaction transaction)
+        {
+            Console.WriteLine($"[BankTransfer] Processed {transaction.Amount:C} for {transaction.Category}");
+        }
+    }
+
+    public class MobileMoneyProcessor : ITransactionProcessor
+    {
+        public void Process(Transaction transaction)
+        {
+            Console.WriteLine($"[MobileMoney] Processed {transaction.Amount:C} for {transaction.Category}");
+        }
+    }
+
+    public class CryptoWalletProcessor : ITransactionProcessor
+    {
+        public void Process(Transaction transaction)
+        {
+            Console.WriteLine($"[CryptoWallet] Processed {transaction.Amount:C} for {transaction.Category}");
+        }
+    }
+
+    // d. Base class Account
+    public class Account
+    {
+        public string AccountNumber { get; set; }
+        public decimal Balance { get; protected set; }
+
+        public Account(string accountNumber, decimal initialBalance)
+        {
+            AccountNumber = accountNumber;
+            Balance = initialBalance;
+        }
+
+        public virtual void ApplyTransaction(Transaction transaction)
+        {
+            Balance -= transaction.Amount;
+        }
+    }
+
+    // e. Sealed SavingsAccount class
+    public sealed class SavingsAccount : Account
+    {
+        public SavingsAccount(string accountNumber, decimal initialBalance)
+            : base(accountNumber, initialBalance)
+        {
+        }
+
+        public override void ApplyTransaction(Transaction transaction)
+        {
+            if (transaction.Amount > Balance)
+            {
+                Console.WriteLine("Insufficient funds");
+            }
+            else
+            {
+                base.ApplyTransaction(transaction);
+                Console.WriteLine($"New Balance: {Balance:C}");
+            }
+        }
+    }
+
+    // f. FinanceApp class
+    public class FinanceApp
+    {
+        private List<Transaction> _transactions = new List<Transaction>();
+
+        public void Run()
+        {
+            // i. Instantiate a SavingsAccount
+            var account = new SavingsAccount("ACCT12345", 1000m);
+
+            // ii. Create transactions
+            var t1 = new Transaction(1, DateTime.Now, 150m, "Groceries");
+            var t2 = new Transaction(2, DateTime.Now, 300m, "Utilities");
+            var t3 = new Transaction(3, DateTime.Now, 500m, "Entertainment");
+
+            // iii. Process each transaction
+            ITransactionProcessor mobile = new MobileMoneyProcessor();
+            ITransactionProcessor bank = new BankTransferProcessor();
+            ITransactionProcessor crypto = new CryptoWalletProcessor();
+
+            mobile.Process(t1);
+            bank.Process(t2);
+            crypto.Process(t3);
+
+            // iv. Apply each transaction
+            account.ApplyTransaction(t1);
+            account.ApplyTransaction(t2);
+            account.ApplyTransaction(t3);
+
+            // v. Add to _transactions list
+            _transactions.Add(t1);
+            _transactions.Add(t2);
+            _transactions.Add(t3);
+        }
+    }
+
+    // Main method
+    class Program
+    {
+        static void Main()
+        {
+            var app = new FinanceApp();
+            app.Run();
+        }
+    }
+}
